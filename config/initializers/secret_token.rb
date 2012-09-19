6a6dec90aed575593b64beed238a33696f23c99f
Ken Winter <kwinter@brighttalk.com>
2012-09-19 23:25:48 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '4d23337ee78902b1e738ced0558912e1451586ba8cdb7b54deff5f3d0486d19f0a9649c1e4ce2e0847a6c494e12ba602ecdf34ca03172952379572ffccf54205'
